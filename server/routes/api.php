c18faadb765c089be7f2845de8301806bdb00566
Ibrahim-Alrbye <77226978+Ibrahim-Alrbye@users.noreply.github.com>
2022-06-20 12:59:31 +0300

    add  new route api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ClucAgeController;
+use App\Http\Controllers\RegisterOrLoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/age', [ClucAgeController::class, 'clucAndSave']);
+
+Route::post('/register', [RegisterOrLoginController::class, 'register']);
+// if login you can use this rotes
+Route::group(['middleware' => 'checkUser'], function () {
+
 });
