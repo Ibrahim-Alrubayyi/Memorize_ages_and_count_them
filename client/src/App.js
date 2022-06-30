d725c0b73de5ecfedc9e51e08e89fd8dba0f2152
Ibrahim-Alrbye <77226978+Ibrahim-Alrbye@users.noreply.github.com>
2022-06-30 20:01:37 +0300

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,39 @@
+import * as React from "react";
+
+//Pages
+import Index from "../src/pages/ageCalculator/Index";
+
+// components
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import { ThemeProvider } from "react-bootstrap";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider dir="rtl">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Index />} />
+
+            <Route path="/login" element={<div>LOGIN</div>} />
+            <Route path="/register" element={<Index />} />
+
+            <Route path="/profile" element={<Index />} />
+            <Route path="/profile/edit" element={<Index />} />
+
+            <Route path="/friend" element={<Index />} />
+            <Route path="/friend/edit" element={<Index />} />
+            <Route path="/friend/add" element={<Index />} />
+
+            <Route path={"*"} element={<Navigate to="/" />} />
+          </Routes>
+          <Footer />
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
