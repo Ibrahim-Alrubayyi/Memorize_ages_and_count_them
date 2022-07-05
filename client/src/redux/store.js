e198b12ee46c6f6371c5f7172d975ee27e174ae6
Ibrahim-Alrbye <77226978+Ibrahim-Alrbye@users.noreply.github.com>
2022-07-05 23:24:16 +0300

    add new reducers

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./Reducers/Reducers";
 import reduxThunk from "redux-thunk";
-// const enhancer =
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
+
+const enhancer = composeEnhancers(applyMiddleware(reduxThunk));
+
+const store = createStore(reducers, enhancer);
 
 export default store;
